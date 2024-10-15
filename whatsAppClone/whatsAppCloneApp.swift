261b7a46956c212fa4201bd3718ea76b5a702ffd
Abdullah <abdullaharslan1473@gmail.com>
2024-10-15 13:40:12 +0300

    Initial Commit

---
+++ whatsAppClone/whatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsAppCloneApp.swift
+//  whatsAppClone
+//
+//  Created by abdullah on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct whatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
